47d96a59a180171521dcc09b4a46073b12efaacb
zintrulcre <zintrulcre@gmail.com>
2019-03-31 09:26:51 +1100

    update

--- CPP-Template/OJ_Entrance.cpp
+++ CPP-Template/OJ_Entrance.cpp
@@ -1,106 +1,30 @@
-//#include <iostream>
-//#include <cmath>
-//#include <queue>
-//#include <vector>
-//#include <string>
-//#include <limits.h> // INT_MAX
-//#include <algorithm>
-//
-//int dir_x[4] = {-1, 0, 1, 0};
-//int dir_y[4] = {0, -1, 0, 1};
-//int R, C;
-//std::vector<std::vector<int>> grid;
-//
-//int BFS(int x, int y, std::queue<std::pair<int, int>> &que) {
-//    int max_time = 0;
-//    int size = que.size(), time = 1;
-//    while (!que.empty()) {
-//        x = que.front().first, y = que.front().second;
-//        que.pop();
-//        max_time = std::max(max_time, grid[x][y]);
-//        for (int i = 0; i < 4; ++i) {
-//            int new_x = x + dir_x[i], new_y = y + dir_y[i];
-//            if (new_x >= 0 && new_x < R && new_y >= 0 && new_y < C && grid[new_x][new_y] == -1) {
-//                que.push(std::pair<int, int>(new_x, new_y));
-//                grid[new_x][new_y] = time;
-//            }
-//        }
-//        --size;
-//        if (size == 0) {
-//            size = que.size();
-//            ++time;
-//        }
-//    }
-//    return max_time;
-//}
-//
-//bool CanDeliver(int &max_time) {
-//    bool deliver = true;
-//    int left_low = INT_MAX, left_high = INT_MIN, right_low = INT_MAX, right_high = INT_MIN;
-//    for (int i = 0; i < R; ++i) {
-//        for (int j = 0; j < C; ++j) {
-//            if (grid[i][j] > max_time) {
-//                deliver = false;
-//                left_low = std::min(left_low, i + j + max_time);
-//                left_high = std::max(left_high, i + j - max_time);
-//                right_low = std::min(right_low, i - j + max_time);
-//                right_high = std::max(right_high, i - j - max_time);
-//            }
-//        }
-//    }
-//    if (deliver)
-//        return true;
-//    for (int i = 0; i < R; ++i) {
-//        for (int j = 0; j < C; ++j) {
-//            int left = i + j, right = i - j;
-//            if (left_high <= left && left <= left_low && right_high <= right && right <= right_low)
-//                return true;
-//        }
-//    }
-//    return false;
-//}
-//
-//int main() {
-//    int num_case = 1;
-//    int T;
-//    std::cin >> T;
-//    for (int t = 0; t < T; ++t) {
-//        std::cin >> R >> C;
-//        grid = std::vector<std::vector<int>>(R, std::vector<int>(C, -1));
-//        std::queue<std::pair<int, int>> que;
-//        for (int i = 0; i < R; ++i) {
-//            std::string temp_in;
-//            std::cin >> temp_in;
-//            for (int j = 0; j < C; ++j) {
-//                char &temp = temp_in[j];
-//                if (temp == '1') {
-//                    que.push(std::pair<int, int>(i, j));
-//                    grid[i][j] = 0;
-//                }
-//            }
-//        }
-//        int size = que.size();
-//        if (size == R * C) {
-//            std::cout << "Case #" + std::to_string(num_case) + ": 0" << std::endl;
-//            ++num_case;
-//            continue;
-//        }
-//
-//        // BFS
-//        int max_time = BFS(0, 0, que);
-//
-//        // Binary Search
-//        int lowest_time = 0, highest_time = INT_MAX;
-//        while (lowest_time < highest_time) {
-//            int mid_time = lowest_time + ((highest_time - lowest_time) >> 1);
-//            if (CanDeliver(mid_time))
-//                highest_time = mid_time;
-//            else
-//                lowest_time = mid_time + 1;
-//        }
-//
-//        std::cout << "Case #" + std::to_string(num_case) + ": " + std::to_string(highest_time) << std::endl;
-//        ++num_case;
-//    }
-//    return 0;
-//}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Func(int n, vector<int> &parameters, int k) {
+    if (n == 0 && k <= static_cast<int>(parameters.size()))
+        return 1;
+    else if (k >= static_cast<int>(parameters.size()))
+        return 0;
+    int curr = n / parameters[k];
+    int count = 0;
+    for(int i = 0; i <= curr; ++i) {
+        int temp = n - i * parameters[k];
+        if (temp < 0)
+            break;
+        count += Func(temp, parameters, k + 1);
+    }
+    return count;
+}
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> parameters{5,2,1};
+    int res = Func(n, parameters, 0);
+    cout << res << endl;
+    return 0;
+}
--- CPP-Template/Solution.h
+++ CPP-Template/Solution.h
@@ -9,71 +9,19 @@
 
 class Solution {
 public:
-    TreeNode *deleteNode(TreeNode *root, int key) {
-        if (!root)
-            return nullptr;
-        auto ret = root;
-        if (root->val == key) {
-            if (!root->right)
-                return root->left;
-            else if (!root->left)
-                return root->right;
-            auto right_left = root->right;
-            while (right_left->left)
-                right_left = right_left->left;
-            right_left->left = root->left->right;
-            root->left->right = root->right;
-            return ret;
+    int Fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+        int prev = 1, curr = 1, i = 2;
+        while (i < n) {
+            int temp = curr;
+            curr += prev;
+            prev = temp;
+            ++i;
         }
-        auto prev = root;
-        while (root) {
-            if (root->val < key) {
-                prev = root;
-                root = root->right;
-            } else if (root->val > key) {
-                prev = root;
-                root = root->left;
-            } else {
-                if (root == prev->left) {
-                    if (!root->left && !root->right) {
-                        prev->left = nullptr;
-                        return ret;
-                    } else if (!root->left) {
-                        prev->left = root->right;
-                        return ret;
-                    } else if (!root->right) {
-                        prev->left = root->left;
-                        return ret;
-                    }
-                    auto right_left = root->right;
-                    while (right_left->left)
-                        right_left = right_left->left;
-                    right_left->left = root->left->right;
-                    root->left->right = root->right;
-                    prev->left = root->left;
-                    return ret;
-                } else {
-                    if (!root->left && !root->right) {
-                        prev->left = nullptr;
-                        return ret;
-                    } else if (!root->left) {
-                        prev->left = root->right;
-                        return ret;
-                    } else if (!root->right) {
-                        prev->left = root->left;
-                        return ret;
-                    }
-                    auto right_left = root->right;
-                    while (right_left->left)
-                        right_left = right_left->left;
-                    right_left->left = root->left->right;
-                    root->left->right = root->right;
-                    prev->right = root->left;
-                    return ret;
-                }
-            }
-        }
-        return nullptr;
+        return curr;
     }
 };
 
