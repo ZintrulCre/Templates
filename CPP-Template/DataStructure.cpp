5950b09ab8679458339db75a453364a27f395d3a
Zhengyu Chen <zintrulcre@gmail.com>
2019-08-12 16:40:41 +1000

    Remove CPP-Template

--- CPP-Template/DataStructure.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by ZintrulCre on 2019-01-26.
-//
-
-#include "DataStructure.h"
-
-bool operator<(const Interval &interval1, const Interval &interval2) {
-    return interval1.start < interval2.start;
-}
