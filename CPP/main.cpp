aa575bf60f6d339d06f8416e66e103d617035cb2
zintrulcre <zintrulcre@gmail.com>
2019-09-18 10:04:05 +1000

    update

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,127 +1,127 @@
-#include <zconf.h>
-#include "Sort/Sort.h"
-#include "Include.h"
-#include "Printer.h"
-#include "Solution.h"
-#include "DataStructure.h"
-#include "TreeSerialization.h"
-#include "Pointer/Include.h"
-
-int main() {
-//    Deleter del;
-//    Object *o = new Object();
-//    int *x = new int(1);
-//    UniquePointer<int, Deleter> u1(x, del);
-//    UniquePointer<int, Deleter> u2(move(u1));
-
-    vector<pair<int, int>> vp = {
-            pair<int, int>{1, 0},
-            pair<int, int>{0, 1}
-    };
-    vector<pair<string, string>> equations = {
-            {"a", "b"},
-            {"e", "f"},
-            {"b", "e"}
-    };
-    vector<double> values = {3.4, 1.4, 2.3};
-    vector<pair<string, string>> queries = {
-            {"b", "a"},
-            {"a", "f"},
-            {"f", "f"},
-            {"e", "e"},
-            {"c", "c"},
-            {"a", "c"},
-            {"f", "e"}
-    };
-//    auto a = new TreeLinkNode(1);
-//    auto b = new TreeLinkNode(2);
-//    auto c = new TreeLinkNode(3);
-//    auto d = new TreeLinkNode(4);
-//    auto e = new TreeLinkNode(5);
-//    auto f = new TreeLinkNode(6);
-//    auto g = new TreeLinkNode(7);
+//#include <zconf.h>
+//#include "Sort/Sort.h"
+//#include "Include.h"
+//#include "Printer.h"
+//#include "Solution.h"
+//#include "DataStructure.h"
+//#include "TreeSerialization.h"
+//#include "Pointer/Include.h"
+//
+//int main() {
+////    Deleter del;
+////    Object *o = new Object();
+////    int *x = new int(1);
+////    UniquePointer<int, Deleter> u1(x, del);
+////    UniquePointer<int, Deleter> u2(move(u1));
+//
+//    vector<pair<int, int>> vp = {
+//            pair<int, int>{1, 0},
+//            pair<int, int>{0, 1}
+//    };
+//    vector<pair<string, string>> equations = {
+//            {"a", "b"},
+//            {"e", "f"},
+//            {"b", "e"}
+//    };
+//    vector<double> values = {3.4, 1.4, 2.3};
+//    vector<pair<string, string>> queries = {
+//            {"b", "a"},
+//            {"a", "f"},
+//            {"f", "f"},
+//            {"e", "e"},
+//            {"c", "c"},
+//            {"a", "c"},
+//            {"f", "e"}
+//    };
+////    auto a = new TreeLinkNode(1);
+////    auto b = new TreeLinkNode(2);
+////    auto c = new TreeLinkNode(3);
+////    auto d = new TreeLinkNode(4);
+////    auto e = new TreeLinkNode(5);
+////    auto f = new TreeLinkNode(6);
+////    auto g = new TreeLinkNode(7);
+////    a->left = b;
+////    a->right = c;
+////    b->left = d;
+////    b->right = e;
+////    c->left = f;
+////    c->right = g;
+////    vector<Interval> a = {Interval(3, 5), Interval(9, 20)};
+////    vector<Interval> b = {Interval(4, 5), Interval(7, 10), Interval(11, 12), Interval(14, 15), Interval(16, 20)};
+////    auto a = new RandomListNode(1);
+////    auto b = new RandomListNode(2);
+////    auto c = new RandomListNode(3);
+////    auto d = new RandomListNode(4);
+////    auto e = new RandomListNode(5);
+////    a->next = b;
+////    b->next = c;
+////    c->next = d;
+////    d->next = e;
+////    a->random = c;
+////    b->random = e;
+////    d->random = b;
+////
+//    auto a = new TreeNode(1);
+//    auto b = new TreeNode(2);
+//    auto c = new TreeNode(3);
+//    auto d = new TreeNode(4);
+////    auto e = new TreeNode(5);
+////    auto f = new TreeNode(6);
+////    auto g = new TreeNode(7);
+////    auto h = new TreeNode(1);
+////    auto i = new TreeNode(5);
+////    auto j = new TreeNode(3);
 //    a->left = b;
 //    a->right = c;
 //    b->left = d;
-//    b->right = e;
-//    c->left = f;
-//    c->right = g;
-//    vector<Interval> a = {Interval(3, 5), Interval(9, 20)};
-//    vector<Interval> b = {Interval(4, 5), Interval(7, 10), Interval(11, 12), Interval(14, 15), Interval(16, 20)};
-//    auto a = new RandomListNode(1);
-//    auto b = new RandomListNode(2);
-//    auto c = new RandomListNode(3);
-//    auto d = new RandomListNode(4);
-//    auto e = new RandomListNode(5);
-//    a->next = b;
-//    b->next = c;
-//    c->next = d;
-//    d->next = e;
-//    a->random = c;
-//    b->random = e;
-//    d->random = b;
+////    b->right = e;
+////    c->left = f;
+////    c->right = g;
+////    d->right = h;
+////    f->left = i;
+////    f->right = j;
 //
-    auto a = new TreeNode(1);
-    auto b = new TreeNode(2);
-    auto c = new TreeNode(3);
-    auto d = new TreeNode(4);
-//    auto e = new TreeNode(5);
-//    auto f = new TreeNode(6);
-//    auto g = new TreeNode(7);
-//    auto h = new TreeNode(1);
-//    auto i = new TreeNode(5);
-//    auto j = new TreeNode(3);
-    a->left = b;
-    a->right = c;
-    b->left = d;
-//    b->right = e;
-//    c->left = f;
-//    c->right = g;
-//    d->right = h;
-//    f->left = i;
-//    f->right = j;
-
-//    auto a = new ListNode(1);
-//    auto b = new ListNode(2);
-//    auto c = new ListNode(3);
-//    auto d = new ListNode(4);
-//    auto e = new ListNode(5);
-//    auto f = new ListNode(2);
-//    auto g = new ListNode(7);
-//    a->next = b;
-//    b->next = c;
-//    c->next = d;
-//    d->next = e;
-//    e->next = f;
-//    f->next = g;
-
-    vector<vector<int>> array{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                              {0, 0, 0, 1, 1, 0, 0, 1, 1, 0},
-                              {0, 1, 0, 0, 1, 0, 1, 1, 0, 1},
-                              {0, 0, 0, 1, 0, 1, 0, 1, 1, 1},
-                              {0, 1, 0, 0, 1, 0, 0, 1, 1, 0},
-                              {0, 1, 0, 1, 0, 0, 0, 1, 1, 0},
-                              {0, 1, 1, 0, 0, 1, 1, 0, 0, 1},
-                              {1, 0, 1, 1, 0, 1, 0, 1, 0, 1}};
-    vector<int> nums = {3, 2, 4, 1}, num1{4, 5, 2, 6, 7, 3, 1};
-    string str1 = "1.0", str2 = "1.0.0";
-    vector<string> strings{"5", "2", "C", "D", "+"}, strings2{"oath", "eat"};
-    vector<vector<int>> matrix = {{0, 1, 1},
-                                  {0, 2, 5},
-                                  {1, 2, 1},
-                                  {2, 3, 1}}, matrix1 = {{1, 2},
-                                                         {2, 3},
-                                                         {3, 1}};
-
-    vector<vector<char>> vc = {{'1', '0', '1', '0', '0'},
-                               {'1', '0', '1', '1', '1'},
-                               {'1', '1', '1', '1', '1'},
-                               {'1', '0', '0', '1', '0'}};
-    Solution sol;
-    auto s = sol.compareVersion(str1, str2);
-    Print(s);
-
-    return 0;
-}
+////    auto a = new ListNode(1);
+////    auto b = new ListNode(2);
+////    auto c = new ListNode(3);
+////    auto d = new ListNode(4);
+////    auto e = new ListNode(5);
+////    auto f = new ListNode(2);
+////    auto g = new ListNode(7);
+////    a->next = b;
+////    b->next = c;
+////    c->next = d;
+////    d->next = e;
+////    e->next = f;
+////    f->next = g;
+//
+//    vector<vector<int>> array{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                              {0, 0, 0, 1, 1, 0, 0, 1, 1, 0},
+//                              {0, 1, 0, 0, 1, 0, 1, 1, 0, 1},
+//                              {0, 0, 0, 1, 0, 1, 0, 1, 1, 1},
+//                              {0, 1, 0, 0, 1, 0, 0, 1, 1, 0},
+//                              {0, 1, 0, 1, 0, 0, 0, 1, 1, 0},
+//                              {0, 1, 1, 0, 0, 1, 1, 0, 0, 1},
+//                              {1, 0, 1, 1, 0, 1, 0, 1, 0, 1}};
+//    vector<int> nums = {3, 2, 4, 1}, num1{4, 5, 2, 6, 7, 3, 1};
+//    string str1 = "1.0", str2 = "1.0.0";
+//    vector<string> strings{"5", "2", "C", "D", "+"}, strings2{"oath", "eat"};
+//    vector<vector<int>> matrix = {{0, 1, 1},
+//                                  {0, 2, 5},
+//                                  {1, 2, 1},
+//                                  {2, 3, 1}}, matrix1 = {{1, 2},
+//                                                         {2, 3},
+//                                                         {3, 1}};
+//
+//    vector<vector<char>> vc = {{'1', '0', '1', '0', '0'},
+//                               {'1', '0', '1', '1', '1'},
+//                               {'1', '1', '1', '1', '1'},
+//                               {'1', '0', '0', '1', '0'}};
+//    Solution sol;
+//    auto s = sol.compareVersion(str1, str2);
+//    Print(s);
+//
+//    return 0;
+//}
