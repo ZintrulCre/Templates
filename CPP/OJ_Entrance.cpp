6bd19bda30854a915be45c400c3d3a9bda9ad8bf
Zhengyu Chen <zintrulcre@gmail.com>
2019-09-26 13:05:33 +1000

    Update OJ_Entrance.cpp

--- CPP/OJ_Entrance.cpp
+++ CPP/OJ_Entrance.cpp
@@ -17,9 +17,24 @@
 using namespace std;
 
 int main() {
-    vector<vector<int>> matrxi{{0, 1, 0, 1, 1},
-                               {1, 1, 0, 0, 1},
-                               {0, 1, 1, 1, 0},
-                               {1, 0, 0, 1, 0}};
-    int m
+    int T;
+    cin >> T;
+    for (int ttt = 0; ttt < T; ++ttt) {
+        int n, m;
+        cin >> n >> m;
+        vector<int> w(m), c(m), res(n, 0), xx(n, 4);
+        for (int i = 0; i < m; ++i)
+            cin >> w[i];
+        for (int i = 0; i < m; ++i) {
+            cin >> c[i];
+            res[w[i] - 1] = c[i];
+            xx[w[i] - 1] = 1;
+        }
+        long long d = 4;
+        for (int i = 1; i < n; ++i) {
+            d = d * 3 % 1000000007;
+        }
+        cout << d << endl;
+    }
+    return 0;
 }
